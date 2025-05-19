7320f8e9ce741b1d03f3cbbda464a5a759ca0994
Romain Talleu <romaintalleu44@gmail.com>
2025-05-19 14:29:58 +0200

    Add SwiftUI basic

---
+++ 2025-05_iOS-101-SwiftUI-basic/pokecard/pokecard/Application/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  pokecard
+//
+//  Created by Romain Talleu on 14/05/2025.
+//
+
+import Foundation
+import SwiftUI
+
+struct ContentView: View {
+	var body: some View {
+		Text("Hello, World!")
+	}
+}
+
+#Preview {
+	ContentView()
+}
