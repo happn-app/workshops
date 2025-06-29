115db48f04b6b890a72a42051a083d260fc2bdd9
Cyril Leroux <cyril0leroux@gmail.com>
2025-06-29 22:36:08 +0200

    Android: Deprecate Pokemon class and update build.gradle.kts to include shared project dependency

--- 2025-06_KMP-101/android/src/main/java/com/happn/android101/domain/Pokemon.kt
+++ 2025-06_KMP-101/android/src/main/java/com/happn/android101/domain/Pokemon.kt
@@ -1,7 +1,8 @@
 package com.happn.android101.domain
 
+@Deprecated(message = "Replace with KMP version", replaceWith = ReplaceWith("com.happn.kmp101.domain.Pokemon"))
 class Pokemon(
     val id: Int,
     val name: String,
-    val holo: Boolean
+    val holo: Boolean,
 )
