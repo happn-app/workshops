b261553ccc9355657732ba8f22bd8f826c1aaf80
Cyril Leroux <cyril0leroux@gmail.com>
2025-06-29 22:26:08 +0200

    Shared: Delete unused classes

--- 2025-06_KMP-101/shared/src/androidMain/kotlin/com/happn/kmp101/Platform.android.kt
+++
@@ -1,7 +0,0 @@
-package com.happn.kmp101
-
-class AndroidPlatform : Platform {
-    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatform(): Platform = AndroidPlatform()
--- 2025-06_KMP-101/shared/src/commonMain/kotlin/com/happn/kmp101/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.happn.kmp101
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
